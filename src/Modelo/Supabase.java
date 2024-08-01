f20a8aeb8a63332e613053386f706cd85f7f8dbf
User <User@CristianT-Pc>
2024-08-01 12:17:48 -0500

    sin credentials

--- src/Modelo/Supabase.java
+++ src/Modelo/Supabase.java
@@ -1,8 +1,8 @@
 package Modelo;
 
 public class Supabase {
-    static private final String SUPABASEURL = "https://fdluzlrlupynnycykfyf.supabase.co/";
-    static private final String ANONKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkbHV6bHJsdXB5bm55Y3lrZnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMxMjgsImV4cCI6MjAzODA0OTEyOH0.gwl3a89XNyzKCFJ1uQS1NTvi8ywef1IiW09gAA3pLBc";
+    static private final String SUPABASEURL = "";
+    static private final String ANONKEY = "";
 
     public Supabase(){
     }
