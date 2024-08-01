edb1d53dff0083886777346269fe41e557519034
User <User@CristianT-Pc>
2024-08-01 15:25:20 -0500

    Añadido Supabase y subida de datos + visuales zoologico cambio de ventanas

--- src/Visual/Visual_Animales.java
+++ src/Visual/Visual_Animales.java
@@ -115,7 +115,8 @@ public class Visual_Animales extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        Visual_GenerarAnimales GenAn = new Visual_GenerarAnimales();
+        GenAn.show(rootPaneCheckingEnabled);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
